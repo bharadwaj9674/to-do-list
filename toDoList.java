561a64e4b12af7c4ead9e3146998f3fab9fc08d1
Bharadwaj Madiraju <91402980+bharadwaj9674@users.noreply.github.com>
2022-07-03 12:19:42 +0530

    Add files via upload

--- toDoList.java
+++ toDoList.java
@@ -1,42 +1,58 @@
 import java.util.*;
+
 import java.io.*;
 
 public class toDoList {
-	
-	Node head;
+
+    Node head;
+
     class Node {
-    	String data;
+        String data;
         int priority;
         Node next;
-        Node(String data, int a){
+
+        Node(String data, int a) {
             this.data = data;
-            this.priority = a;  
+            this.priority = a;
         }
     }
-    
-    public void getMin(){
-        System.out.println("Task to be completed first:"+" "+head.data);
+
+    public void updateFile() throws IOException{
+        FileWriter fw = new FileWriter("/Users/bharath/Desktop/DSA_Project/data.txt");
+        Node x = head;
+        while(x != null){
+            x=x.next;
+            fw.write(x.data+","+x.priority);
+        }
+
     }
-    
-    public void removeMin(){
-        Node temp = head;
-        System.out.println("Removing the first task completed:"+" "+head.data);
-        head = head.next;
-        temp.next = null;
+
+
+    public void getMin() {
+        System.out.println("Task to be completed first:" + " " + head.data);
+
     }
-    
-    public void  push(String data, int priority){
+
+    public void removeMin() throws IOException {
+         Node temp = head;
+         System.out.println("Removing the first task completed:"+" "+head.data);
+         head = head.next;
+         temp.next = null;
+
+         updateFile();
+
+    }
+
+    public void push(String data, int priority) {
         Node temp = new Node(data, priority);
-        if(head==null){
+        if (head == null) {
             head = temp;
-        }
-        else{
+        } else {
             Node t = head;
             if (head.priority > priority) {
                 temp.next = head;
                 head = temp;
-            }
-            else {
+            } else {
                 while (t.next != null &&
                         t.next.priority < priority) {
                     t = t.next;
@@ -47,67 +63,79 @@ public class toDoList {
             }
         }
     }
-    
-    public boolean isEmpty(){
-        if(head==null){
+
+    public boolean isEmpty() {
+        if (head == null) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    
-    public void Display() throws FileNotFoundException{
-    	File file = new File("/Users/bharath/Desktop/DSA_Project/data.txt");
-    	Scanner scan = new Scanner(file);
-    	while(scan.hasNextLine()) {
-    		String str = scan.nextLine();
-    		String[] arr = str.split(",");
-    		System.out.println("task: "+arr[0]+" --> priority: "+arr[1]);
-    	}
-    	scan.close();
-    }
-    	
-    
-	public static void main(String[]args) throws IOException{
-		
-		FileWriter fileWriter = new FileWriter("/Users/bharath/Desktop/DSA_Project/data.txt",true);
 
-		
-		
-		toDoList toDo = new toDoList();
-		
-		System.out.println("------------To do list------------");
-		System.out.println();
-		System.out.println("1.Show Tasks");
-		System.out.println("2.Remove Task");
-		System.out.println("3.Adding Task");
-		System.out.println();
-		
+    public void Display() {
+
+        Node t = head;
+        while (t != null) {
+            System.out.println("Task: "+t.data + "  ---->  Priority: " + t.priority);
+            t = t.next;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        toDoList toDo = new toDoList();
+
+        File file = new File("/Users/bharath/Desktop/DSA_Project/data.txt");
+        FileReader fr = new FileReader("/Users/bharath/Desktop/DSA_Project/data.txt");
+        BufferedReader bf = new BufferedReader(fr);
+
+        String str;
+
+        while ((str = bf.readLine()) != null) {
+
+            String[] arr = str.split(",");
+            toDo.push(arr[0], Integer.parseInt(arr[1]));
+
+        }
+
+        FileWriter fileWriter = new FileWriter("/Users/bharath/Desktop/DSA_Project/data.txt", true);
+
+        System.out.println("------------To do list------------");
+        System.out.println();
+        System.out.println("1.Show Tasks");
+        System.out.println("2.Remove Task");
+        System.out.println("3.First Task To Be Done");
+        System.out.println("4.Adding Task");
+        System.out.println();
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter one number to do following functions:  ");
-		int a = scan.nextInt();
-		
-		if(a==1) {
-				toDo.Display();
-		}
-		if(a==2) {
-			if(toDo.isEmpty()==true) {
-				System.out.println("There are no tasks to remove.....!");
-			}
-			else {
-				toDo.removeMin();
-			}
-		}
-		
-		if(a==3) {
-			String string = scan.next();
-			int integer = scan.nextInt();
-			toDo.push(string, integer);	
-			fileWriter.write(string+","+integer);
-			fileWriter.write(System.lineSeparator());
-		}
-		scan.close();
-		fileWriter.close();
-	}
+        int a = scan.nextInt();
+
+        if (a == 1) {
+            toDo.Display();
+        }
+        if (a == 2) {
+            if (toDo.isEmpty() == true) {
+                System.out.println("There are no tasks to remove.....!");
+            } else {
+                toDo.removeMin();
+            }
+        }
+
+        if (a == 3) {
+            toDo.getMin();
+        }
+
+        if (a == 4) {
+            String string = scan.next();
+            int integer = scan.nextInt();
+            toDo.push(string, integer);
+            fileWriter.write(string + "," + integer);
+            fileWriter.write(System.lineSeparator());
+        }
+        scan.close();
+        fileWriter.close();
+        bf.close();
+    }
 }
