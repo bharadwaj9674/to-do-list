55758253ac64a216b6c2612bdb17c2855dd1df5f
Bharadwaj Madiraju <91402980+bharadwaj9674@users.noreply.github.com>
2022-07-06 17:51:49 +0530

    Add files via upload

--- toDoList.java
+++ toDoList.java
@@ -29,19 +29,41 @@ public class toDoList {
         fw.close();
     }
     
+    // Method for displaying completed tasks
+    
+    public void tasksCompleted() throws IOException {
+        
+        FileReader fr = new FileReader("/Users/bharath/Desktop/DSA_Project/taskCompleted.txt");
+        BufferedReader br = new BufferedReader(fr);
+        
+        System.out.println(terminalColours.PURPLE_BOLD_BRIGHT+terminalColours.WHITE_BACKGROUND+"Completed tasks list"+terminalColours.RESET);
+        String str;
+        while((str=br.readLine())!=null) {
+            
+            String arr[] = str.split(",");
+            System.out.println(terminalColours.PURPLE_BRIGHT+"Task: "+terminalColours.RESET+" "+arr[0]/*+"   ---->   "+terminalColours.PURPLE_BRIGHT+"Priority: "+terminalColours.RESET+" "+arr[1]*/);
+        }   
+        br.close();
+    }
+    
     // Getting minimum from the file
 
     public void getMin() {
-        System.out.println("Task to be completed first:" + " " + head.data);
+        System.out.println(terminalColours.CYAN_BRIGHT+"Task to be completed first:" +terminalColours.RESET+ "  " + head.data);
     }
 
     public void removeMin() throws IOException {
          Node temp = head;
-         System.out.println("Removing the first task completed:"+" "+head.data);
+         System.out.println(terminalColours.RED_BRIGHT+"Removing the first task completed:"+terminalColours.RESET+"  "+head.data);
+         
+      // Keeping track of completed tasks
+         FileWriter fw = new FileWriter("/Users/bharath/Desktop/DSA_Project/taskCompleted.txt",true);
+         fw.write(head.data + "," + head.priority);
+         fw.write(System.lineSeparator());
+         fw.close();
          head = head.next;
          temp.next = null;
          
-         //FileReader fr = new FileReader("/Users/bharath/Desktop/DSA_Project/taskData.txt");
 
          updateFile();
     }
@@ -101,24 +123,25 @@ public class toDoList {
 
         FileWriter fileWriter = new FileWriter("/Users/bharath/Desktop/DSA_Project/taskData.txt", true);
 
-        System.out.println(terminalColours.WHITE_BACKGROUND+terminalColours.PURPLE_BOLD+"------------To do list------------"+terminalColours.RESET);
+        System.out.println(terminalColours.WHITE_BACKGROUND+terminalColours.BLUE_BOLD+"------------To do list------------"+terminalColours.RESET);
         System.out.println();
         System.out.println(terminalColours.GREEN_BRIGHT+"1.Show Tasks"+terminalColours.RESET);
         System.out.println(terminalColours.RED_BRIGHT+"2.Remove Task"+terminalColours.RESET);
         System.out.println(terminalColours.CYAN_BRIGHT+"3.First Task To Be Done"+terminalColours.RESET);
         System.out.println(terminalColours.YELLOW_BRIGHT+"4.Adding Task"+terminalColours.RESET);
+        System.out.println(terminalColours.PURPLE_BRIGHT+"5.Display completed tasks"+terminalColours.RESET);
         System.out.println();
 
         Scanner scan = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
-        System.out.print(terminalColours.WHITE_BACKGROUND+terminalColours.PURPLE_BOLD+"Enter one number to do following functions:"+terminalColours.RESET+"  ");
+        System.out.print(terminalColours.WHITE_BACKGROUND+terminalColours.BLUE_BOLD+"Enter one number to do following functions:"+terminalColours.RESET+"  ");
         int a = scan.nextInt();
         
         // To display tasks
 
         if (a == 1) {
             if(toDo.isEmpty() == true) {
-                System.out.println("There are no tasks to display....!");
+                System.out.println(terminalColours.RED_BRIGHT+"There are no tasks to display....!"+terminalColours.RESET);
             }
             else {
             toDo.Display();
@@ -129,12 +152,12 @@ public class toDoList {
         
         if (a == 2) {
             if (toDo.isEmpty() == true) {
-                System.out.println("There are no tasks to remove.....!");
+                System.out.println(terminalColours.RED_BRIGHT+"There are no tasks to remove.....!"+terminalColours.RESET);
             } 
             else {
                 
                 
-                System.out.println("Select to remove single task or multiple tasks");
+                System.out.println(terminalColours.RED_BRIGHT+"Select to remove single task or multiple tasks"+terminalColours.RESET);
                 System.out.println();
                 System.out.println("1.Remove Single Task");
                 System.out.println("2.Remove Multiple Tasks");
@@ -148,7 +171,7 @@ public class toDoList {
                 
                 // Multiple tasks removing
                 if(single==2) {
-                    System.out.print("Specify how many tasks to remove:  ");
+                    System.out.print(terminalColours.RED_BRIGHT+"Specify how many tasks to remove:  "+terminalColours.RESET);
                     int multiple = scan.nextInt();
                     for(int i=0;i<multiple;i++) {
                         toDo.removeMin();
@@ -161,7 +184,7 @@ public class toDoList {
 
         if (a == 3) {
             if(toDo.isEmpty() == true) {
-                System.out.println("There are no tasks to do....!");
+                System.out.println(terminalColours.RED_BRIGHT+"There are no tasks to do....!"+terminalColours.RESET);
             }
             else {
                 toDo.getMin();
@@ -206,7 +229,14 @@ public class toDoList {
                     fileWriter.write(string + "," + integer);
                     fileWriter.write(System.lineSeparator());
                 }
-            }   
+            }
+            
+            // Displaying completed tasks
+            
+        }
+        
+        if(a==5) {
+            toDo.tasksCompleted();
         }
         scan.close();
         sc.close();
@@ -214,5 +244,3 @@ public class toDoList {
         bf.close();
     }
 }
-
-// completed tasks
